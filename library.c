1b238ff930b9a2c15a1ef1521b39dd460da4474b
Jake Goldman <jgoldman@stuy.edu>
2017-10-20 11:00:46 -0400

    basic classwork

--- library.c
+++ library.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "linked_list.h"
+#include "library.h"
+
+struct node * player[26];
+
+void add_song( char * song ){
+    int index = song[0] - 97; // using the ascii values to get the index for the table
+    struct node * n = player[ index ];
+    while( n && strcmp( n->data, song ) ){
+        
+    }
+}
